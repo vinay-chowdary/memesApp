dd8ec4f1d74719c7e969d989588150030cfe9efd
vinay-chowdary <vinaychowdary.dev@gmail.com>
2021-02-07 10:22:45 +0530

    small fixes

--- server/server.js
+++ server/server.js
@@ -36,5 +36,5 @@ mongoose.connect('mongodb://localhost:27017/memesDB',
 
 const port = process.env.PORT || 8081;
 app.listen(port, () => {
-    console.log(`server listening on ${port}`);
+    logger.log("info", `server listening on ${port}`);
 });
