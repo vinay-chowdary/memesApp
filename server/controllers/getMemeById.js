4ab785dd39481eeb76ab2a8316b1b0bdb7af0fc0
vinay-chowdary <vinaychowdary.dev@gmail.com>
2021-02-11 16:36:49 +0530

    improved readability

--- server/controllers/getMemeById.js
+++ server/controllers/getMemeById.js
@@ -1,15 +1,24 @@
 const { logger, Meme } = require('./requirements')
 
+
 const getMemeById = async (req, res) => {
+
     try {
         const id = req.params.id;
+
+        //  check if objectId id valid according to mongodb
         if (id.match(/^[0-9a-fA-F]{24}$/)) {
-            const meme = await Meme.findById({ _id: id })
+
+            const meme = await Meme.findById(id)
+
+            // if meme is not found
             if (!meme)
                 throw new Error(JSON.stringify({ status: 404, message: "Meme with requested Id is not found" }))
-            res.status(200).json(meme)
+            else
+                res.status(200).json(meme)
         }
-        throw new Error(JSON.stringify({ status: 404, message: "Invalid object Id" }))
+        else
+            throw new Error(JSON.stringify({ status: 404, message: "Invalid object Id" }))
 
 
     }
