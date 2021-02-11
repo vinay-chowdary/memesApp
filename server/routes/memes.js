081fe1fdf1b25797684a67c8707aaf53cd0049f8
vinay-chowdary <vinaychowdary.dev@gmail.com>
2021-02-11 16:52:19 +0530

    add get and patch routes to meme/id

--- server/routes/memes.js
+++ server/routes/memes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router()
-const { fetchMemes, postMeme, paginate, getMemeById } = require('../controllers')
+const { fetchMemes, postMeme, paginate, getMemeById, patchMeme } = require('../controllers')
 
 router.route('/')
     .get(paginate(), fetchMemes)
     .post(postMeme)
 
 router.route('/:id')
-    .get(getMemeById);
+    .get(getMemeById)
+    .patch(patchMeme)
 
 module.exports = router
