ce6f4089c9fb5c50226c48855a0aba96a0eb3130
vinay-chowdary <vinaychowdary.dev@gmail.com>
2021-02-11 16:48:33 +0530

    increase readability and bug fixes

--- server/controllers/paginate.js
+++ server/controllers/paginate.js
@@ -1,29 +1,40 @@
 const Meme = require('../models/meme.model')
 
 const paginate = () => async (req, res, next) => {
+
     const page = parseInt(req.query.page) || 1;
     const limit = 100;
     const start = (page - 1) * limit
     const paginatedResults = {};
     const totalDocuments = await Meme.countDocuments().exec()
     const totalPages = Math.ceil(totalDocuments / limit);
-    if (page > totalPages) {
+
+
+    paginatedResults.memesPerPage = limit;
+    paginatedResults.startMeme = start;
+    paginatedResults.page = page;
+    paginatedResults.totalPages = totalPages;
+
+
+    //  if there are memes but page number has exceded
+
+    if (totalPages !== 0 && page > totalPages) {
         res.status(404).json({
             message: {
                 reason: "page number exceeded", totalPages: totalPages
             }
         })
     }
+
+    //  if there are no memes or page number doesnot exceed
+
     else {
-        paginatedResults.startMeme = start;
-        paginatedResults.memesPerPage = limit;
-        paginatedResults.totalPages = totalPages;
         if (start > 0) {
             paginatedResults.previous = {
                 page: page - 1
             }
         }
-        if (page !== totalPages) {
+        if (page < totalPages) {
             paginatedResults.next = {
                 page: page + 1
             }
