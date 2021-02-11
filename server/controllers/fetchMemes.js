ba1bfe93367d65cd7f2c758bf8c88b514643b410
vinay-chowdary <vinaychowdary.dev@gmail.com>
2021-02-11 16:32:33 +0530

    improved documentation by adding comments

--- server/controllers/fetchMemes.js
+++ server/controllers/fetchMemes.js
@@ -2,17 +2,31 @@ const { logger, Meme } = require('./requirements')
 
 
 
-// fetch latest 100 memes from database
+// fetch latest 100 memes from database at once
 
-const fetchMemes = async (req, res) => {
+const fetchMemes = async (_, res) => {
+
     try {
-        const { startMeme: start, memesPerPage: limit } = res.results;
+        const { startMeme: start, memesPerPage: limit, totalPages } = res.results;
+
+        //  get 100 memes from database according to start meme(page)
         const memes = await Meme.find({}).limit(limit).skip(start).sort({ '_id': -1 })
+
+
+        // display that Meme start from 1 not 0
         res.results.startMeme += 1;
+
+
         const response = { ...res.results, memes }
-        res.status(200).json(response)
+
+        //  if there are no memes return empty error instead of all other data
+        totalPages !== 0 ? res.status(200).json(response) : res.status(200).json(memes)
+
     }
     catch (err) {
+
+        //  if anything goes wrong it will be internal server error(status:500)
+
         logger.log('error', err.message)
         res.status(500).json({ message: err.message })
     }
