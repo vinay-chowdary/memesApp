c3ea81995cc0350869620096be2eeddfca4dde77
vinay-chowdary <vinaychowdary.dev@gmail.com>
2021-02-14 23:32:00 +0530

    add scripts

--- install.sh
+++ install.sh
@@ -3,5 +3,14 @@
 cd ./server
 
 sudo apt update
+sudo apt upgrade -y
+sudo apt install -y docker.io
+
+# Using Ubuntu nodejs install
+curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+sudo apt install nodemon
+
 npm install
 docker run -d -p 27107:27107 --name  mongoDB mongo
